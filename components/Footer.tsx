3f88f387289a3677d459e697d67056938bc9dcc9
BlueTeamAfrica <eldabyk@gmail.com>
2025-12-19 19:10:47 +0200

    Remove unused Image import from Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import Link from 'next/link'
-import Image from 'next/image'
 import { Phone, Mail, MapPin, Facebook, X, Linkedin, Instagram, Youtube } from 'lucide-react'
 
 export default function Footer() {
