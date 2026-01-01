239e27d5f9653ac442dc1b7b79fa919253805835
BlueTeamAfrica <eldabyk@gmail.com>
2026-01-01 18:58:27 +0200

    Fix blog images not displaying - remove contentDispositionType attachment setting

--- next.config.js
+++ next.config.js
@@ -30,7 +30,6 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     // Enable image optimization
     dangerouslyAllowSVG: true, // Allow SVG for simpleicons
-    contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   // Enable compression
